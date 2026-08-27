e588a83682232527e38ac1a83f7984a1c302dcc8
Laura <laura@wiese2.org>
2026-08-27 22:12:41 +0200

    add windows resource metadata

--- resources.go
+++ resources.go
@@ -2,4 +2,4 @@
 
 package main
 
-//go:generate go run github.com/tc-hib/go-winres@v0.3.3 simply --arch amd64,arm64 --out rsrc --manifest gui --product-name CatLock --file-description "A keyboard lock for cats" --icon assets/catlock.png
+//go:generate go run github.com/tc-hib/go-winres@v0.3.3 make --in winres/winres.json --arch amd64,arm64 --out rsrc
